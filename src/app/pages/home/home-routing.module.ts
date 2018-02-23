014b48b15a79bfc153a01a5b8101f16aac2b252c
deepthan <deepthan@163.com>
2018-02-23 16:09:41 +0800

    ok

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './home.component';
 
 const routes: Routes = [
@@ -14,4 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
 export class HomeRoutingModule { }
